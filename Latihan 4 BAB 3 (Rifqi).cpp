ac19b05de174a5fea8bd5d020ec49117c1509cfb
Xanthippe <43535349+Xanthippee@users.noreply.github.com>
2018-10-05 02:19:47 +0700

    Update Latihan 4 BAB 3 (Rifqi).cpp

--- Latihan 4 BAB 3 (Rifqi).cpp
+++ Latihan 4 BAB 3 (Rifqi).cpp
@@ -9,9 +9,9 @@ int main ()
  cout<<"Masukkan huruf kecil : "; 
  cin>>huruf; cout<<endl;
  //seleksi pembanding OR, salah satu kondisi bernilai benar
- if ((huruf == "a") || (huruf == "i") ||
-    (huruf == "u") || ()huruf == "e")|| 
-    (huruf == "o")) {
+ if ((huruf == 'a') || (huruf == 'i') ||
+    (huruf == 'u') || (huruf == 'e')|| 
+    (huruf == 'o')) {
   cout<<"Anda memasukkan huruf vokal";
   cout<<"Yang Anda masukkan adalah huruf konsonan";
  
